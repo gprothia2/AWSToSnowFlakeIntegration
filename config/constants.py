4311000090bd1dc443fefbe37d78496e429e5428
gprothia2 <81781486+gprothia2@users.noreply.github.com>
2022-05-27 08:44:48 -0700

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+metadata_table = << Provide name of the metadata registry table created in Step 1 >> 
+log_table = << Provide ame of the log table created in Step 1 >> 
+trigger_file = << Provide name of the trigger file >>
+
+ 
+#Snowflake connection details
+account = <<Provide nameof Snowflake DB Account >>
+user =<< Provide name of Snowflake DB User >>
+password =<< Provide name of Snowflake DB Password >>
+database = << Provide name of Snowflake DB database >>
+schema = << Provide name of Snowflake DB Schema >>
+warehouse =<< Provide name of Snowflake DB wraehose >>'
+port=443
+role=<< Provide name of Snowflake DB sysadmin role >>
+
+con=(user, password, account, warehouse, database, port)
